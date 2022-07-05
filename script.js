fdaa8b3dbe5bcdcdc75151400f3f042cae83741b
OpticDesignPro <108537254+OpticDesignPro@users.noreply.github.com>
2022-07-05 17:07:39 -0400

    fix slideshows, full page reset

--- script.js
+++ script.js
@@ -1,72 +1,146 @@
 // Slideshow auto scrolling for reviews
+var slideIndex = 1;
 
-let slideIndex = 1;
-showSlides(slideIndex);
+var myTimer;
 
-// Next/previous controls
-// function plusSlides(n) {
-//     showSlides(slideIndex += n);
-// }
+var slideshowContainer;
 
-// Thumbnail image controls
+window.addEventListener("load", function () {
+    showSlides(slideIndex);
+    myTimer = setInterval(function () { plusSlides(1) }, 9000);
+
+    //COMMENT OUT THE LINE BELOW TO KEEP ARROWS PART OF MOUSEENTER PAUSE/RESUME
+    slideshowContainer = document.getElementsByClassName('slideshow-inner')[0];
+
+    //UNCOMMENT OUT THE LINE BELOW TO KEEP ARROWS PART OF MOUSEENTER PAUSE/RESUME
+    // slideshowContainer = document.getElementsByClassName('slideshow-container')[0];
+
+    slideshowContainer.addEventListener('mouseenter', pause)
+    slideshowContainer.addEventListener('mouseleave', resume)
+})
+
+// NEXT AND PREVIOUS CONTROL
+function plusSlides(n) {
+    clearInterval(myTimer);
+    if (n < 0) {
+        showSlides(slideIndex -= 1);
+    } else {
+        showSlides(slideIndex += 1);
+    }
+
+    //COMMENT OUT THE LINES BELOW TO KEEP ARROWS PART OF MOUSEENTER PAUSE/RESUME
+
+    if (n === -1) {
+        myTimer = setInterval(function () { plusSlides(n + 2) }, 9000);
+    } else {
+        myTimer = setInterval(function () { plusSlides(n + 1) }, 9000);
+    }
+}
+
+//Controls the current slide and resets interval if needed
 function currentSlide(n) {
+    clearInterval(myTimer);
+    myTimer = setInterval(function () { plusSlides(n + 1) }, 9000);
     showSlides(slideIndex = n);
 }
 
 function showSlides(n) {
-    let i;
-    let slides = document.getElementsByClassName("mySlides");
-    let dots = document.getElementsByClassName("dot");
+    var i;
+    var slides = document.getElementsByClassName("mySlides");
+    var dots = document.getElementsByClassName("dot");
     if (n > slides.length) { slideIndex = 1 }
     if (n < 1) { slideIndex = slides.length }
     for (i = 0; i < slides.length; i++) {
         slides[i].style.display = "none";
     }
-    slideIndex++;
-    if (slideIndex > slides.length) { slideIndex = 1 }
-    slides[slideIndex - 1].style.display = "block";
     for (i = 0; i < dots.length; i++) {
         dots[i].className = dots[i].className.replace(" active", "");
     }
     slides[slideIndex - 1].style.display = "block";
     dots[slideIndex - 1].className += " active";
-    setTimeout(showSlides, 8000);
+}
+
+pause = () => {
+    clearInterval(myTimer);
+}
+
+resume = () => {
+    clearInterval(myTimer);
+    myTimer = setInterval(function () { plusSlides(slideIndex) }, 9000);
 }
 
 // Slideshow auto scrolling for images
 
-let slideIndex2 = 1;
-showSlides2(slideIndex);
+var slideIndex2 = 1;
 
-// Next/previous controls
+var myTimer2;
+
+var slideshowContainer2;
+
+window.addEventListener("load", function () {
+    showSlides2(slideIndex2);
+    myTimer2 = setInterval(function () { plusSlides2(1) }, 4000);
+
+    //COMMENT OUT THE LINE BELOW TO KEEP ARROWS PART OF MOUSEENTER PAUSE/RESUME
+    slideshowContainer2 = document.getElementsByClassName('slideshow-inner')[0];
+
+    //UNCOMMENT OUT THE LINE BELOW TO KEEP ARROWS PART OF MOUSEENTER PAUSE/RESUME
+    // slideshowContainer = document.getElementsByClassName('slideshow-container')[0];
+
+    slideshowContainer2.addEventListener('mouseenter', pause)
+    slideshowContainer2.addEventListener('mouseleave', resume)
+})
+
+// NEXT AND PREVIOUS CONTROL
 function plusSlides2(n) {
-    showSlides2(slideIndex += n);
+    clearInterval(myTimer2);
+    if (n < 0) {
+        showSlides2(slideIndex2 -= 1);
+    } else {
+        showSlides2(slideIndex2 += 1);
+    }
+
+    //COMMENT OUT THE LINES BELOW TO KEEP ARROWS PART OF MOUSEENTER PAUSE/RESUME
+
+    if (n === -1) {
+        myTimer2 = setInterval(function () { plusSlides2(n + 2) }, 4000);
+    } else {
+        myTimer2 = setInterval(function () { plusSlides2(n + 1) }, 4000);
+    }
 }
 
-// Thumbnail image controls
+//Controls the current slide and resets interval if needed
 function currentSlide2(n) {
-    showSlides2(slideIndex = n);
+    clearInterval(myTimer2);
+    myTimer2 = setInterval(function () { plusSlides2(n + 1) }, 4000);
+    showSlides2(slideIndex2 = n);
 }
 
 function showSlides2(n) {
-    let i;
-    let slides = document.getElementsByClassName("mySlides2");
-    let dots = document.getElementsByClassName("dot2");
-    if (n > slides.length) { slideIndex = 1 }
-    if (n < 1) { slideIndex = slides.length }
-    for (i = 0; i < slides.length; i++) {
-        slides[i].style.display = "none";
+    var i;
+    var slides2 = document.getElementsByClassName("mySlides2");
+    var dots2 = document.getElementsByClassName("dot2");
+    if (n > slides2.length) { slideIndex2 = 1 }
+    if (n < 1) { slideIndex2 = slides2.length }
+    for (i = 0; i < slides2.length; i++) {
+        slides2[i].style.display = "none";
     }
-    slideIndex2++;
-    if (slideIndex2 > slides.length) { slideIndex2 = 1 }
-    slides[slideIndex2 - 1].style.display = "block";
-    for (i = 0; i < dots.length; i++) {
-        dots[i].className = dots[i].className.replace(" active", "");
+    for (i = 0; i < dots2.length; i++) {
+        dots2[i].className = dots2[i].className.replace(" active", "");
     }
-    slides[slideIndex2 - 1].style.display = "block";
-    dots[slideIndex2 - 1].className += " active";
-    setTimeout(showSlides2, 3000);
+    slides2[slideIndex2 - 1].style.display = "block";
+    dots2[slideIndex2 - 1].className += " active";
 }
+
+pause = () => {
+    clearInterval(myTimer2);
+}
+
+resume = () => {
+    clearInterval(myTimer2);
+    myTimer2 = setInterval(function () { plusSlides2(slideIndex2) }, 4000);
+}
+
 
 // Navbar transparency on scroll
 
@@ -119,3 +193,4 @@ $(function () {
         });
     });
 });
+
